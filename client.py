4b5feebf9f2ba83292e2d6270a5329dbaf8e15a8
VenomCrack1337 <zaharnovikov67@gmail.com>
2026-08-09 13:53:12 -0600

    Update SERVER_URL for deployment

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ import psutil
 
 # ⚠️ ЗАМЕНИТЕ ЭТУ СТРОКУ НА ВАШ ДОМЕН RENDER ПОСЛЕ ДЕПЛОЯ (например: https://ваш-проект.onrender.com)
 # Если тестируете локально, оставьте как есть: http://127.0.0.1:5000
-SERVER_URL = 'http://127.0.0.1:5000' 
+SERVER_URL = 'https://webrat-eagn.onrender.com' 
 
 sio = socketio.Client()
 
